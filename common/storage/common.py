1f0fcd837d5e2272aa0a35eb56432a1eff741beb
Greene <greene9832@gmail.com>
2018-10-13 11:45:22 +0800

    add db storage

---
+++ common/storage/common.py
@@ -0,0 +1,42 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+from common.conf import get_db_uri
+
+Base = declarative_base()
+metadata = Base.metadata
+
+DB_URI = get_db_uri().get('DB_URI')
+
+class StorageBase(object):
+    def insert(self, values):
+        for k,v in values.items():
+            setattr(self,k,v)
+
+        session = get_session()
+        session.add(self)
+        session.commit()
+
+    def delete(self, item):
+        pass
+
+    def update(self, item,values):
+        for k,v in values.items():
+            setattr(self,k,v)
+
+        session = get_session()
+
+
+    def query(self, item):
+        pass
+
+def get_engine():
+    engine = create_engine(DB_URI)
+    return engine
+
+def get_session():
+    engine = create_engine(DB_URI)
+    session = sessionmaker(bind=engine)
+
+    return session()
